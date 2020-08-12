4ffcede5814422977140cac29f68644784ee9c69
sun_cx <sun.chengxiong@foxmail.com>
2020-08-12 18:06:48 +0800

    add required annotation...

--- core/tests/BlockingQueue-Test.cpp
+++ core/tests/BlockingQueue-Test.cpp
@@ -2,12 +2,98 @@
 // Created by suncx on 2020/8/10.
 //
 #include "../BlockingQueue.h"
+#include "../CountDownLatch.h"
+#include <memory>
+#include <vector>
+#include <cstring>
+#include <functional>
+
+using std::vector;
+using std::unique_ptr;
+using std::make_unique;
+using std::bind;
+
+/**
+ * 本例是一个典型的生产者/消费者模型：
+ * 主线程生产数据，5 个子线程消费数据
+ */
+class Test {
+private:
+    BlockingQueue<string> queue;
+    CountDownLatch latch;
+    vector<unique_ptr<Thread>> threads;
+
+    void thread_func() {
+        printf("tid: %d, %s started...\n", CurrentThread::get_pid(), CurrentThread::get_name().c_str());
+        latch.count_down();
+        bool running = true;
+        while (running) {
+            string d = queue.de_queue();
+            printf("%s[%d]: consume data: %s, size = %zu\n", CurrentThread::get_name().c_str(),
+                   CurrentThread::get_pid(), d.c_str(), queue.size());
+            running = d != "stop";
+        }
+        printf("%s[%d] stopped...\n", CurrentThread::get_name().c_str(), CurrentThread::get_pid());
+    }
+
+public:
+    explicit Test(int n_threads) : latch(n_threads) {
+        char name[32];
+        for (int i = 0; i < n_threads; ++i) {
+            memset(name, 0, sizeof(name));
+            snprintf(name, sizeof(name), "work-thread-%d", i + 1);
+            threads.emplace_back(new Thread(bind(&Test::thread_func, this), name));
+        }
+        for (const auto &e:threads) e->start();
+    }
+
+    void run(int times) {
+        printf("waiting for count_down_latch...\n");
+        latch.wait();
+        printf("all threads started...\n");
+        char buf[32];
+        for (int i = 0; i < times; ++i) {
+            memset(buf, 0, sizeof(buf));
+            snprintf(buf, sizeof(buf), "hello %d", i + 1);
+            queue.en_queue(buf);
+            printf("%s[%d] enque: %s, queue size: %zu\n", CurrentThread::get_name().c_str(), CurrentThread::get_pid(),
+                   buf, queue.size());
+        }
+    }
+
+    void join_all() {
+        for (int i = 0; i < threads.size(); ++i) {
+            queue.en_queue("stop");
+        }
+        for (const auto &th:threads) th->join();
+    }
+};
+
+void test_move() {
+    BlockingQueue<unique_ptr<int>> queue;
+    queue.en_queue(make_unique<int>(42));
+
+    unique_ptr<int> ptr = queue.de_queue();
+    printf("de_que value: %d\n", *ptr);
+
+    *ptr = 123;
+    queue.en_queue(move(ptr));
+    printf("judge: %d\n", ptr == nullptr);
+
+    unique_ptr<int> y = queue.de_queue();
+    printf("deque value2: %d\n", *y);
+}
 
 int main(int argc, const char *argv[]) {
 
-    BlockingQueue<int> bq;
+    printf("thread name: %s, pid: %d, tid: %d\n", CurrentThread::get_name().c_str(), getpid(),
+           CurrentThread::get_pid());
 
-    printf("bq default size: %zu\n", bq.size());
+    Test t(5);   // 5 个消费者消费数据
+    t.run(100);     // 生产 100 条数据
+    t.join_all();
+
+//    test_move();
 
     return 0;
 }
--- core/tests/CircularBuffer-Test.cpp
+++ core/tests/CircularBuffer-Test.cpp
@@ -9,23 +9,25 @@ int main(int argc, const char *argv[]) {
     CircularBuffer<int> buffer(3);
 
     buffer.push_back(4);
-    cout << buffer;
-    printf("current capacity: %zu\n", buffer.get_capacity());
+    println(buffer);
     buffer.push_back(8);
-    cout << buffer;
-//    printf("current size: %zu\n", buffer.size());
-    buffer.push_back(20);
-    cout << buffer;
-//    printf("current size: %zu\n", buffer.size());
-    buffer.push_back(60);
-    cout << buffer;
-//    printf("current size: %zu\n", buffer.size());
-    buffer.push_back(80);
-    cout << buffer;
-//    printf("current size: %zu\n", buffer.size());
+    println(buffer);
+    buffer.push_back(12);
+    println(buffer);
 
-//    int val = buffer.pop_front();
-//    printf("pop val: %d, current size: %zu\n", val, buffer.size());
+    buffer.push_back(16);
+    println(buffer);
+
+    buffer.pop_front();
+    println(buffer);
+    buffer.pop_front();
+    println(buffer);
+    buffer.pop_front();
+    println(buffer);
+
+    buffer.pop_front();
+    println(buffer);
+
 
     return 0;
 }
