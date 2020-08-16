d4da7293efa95a9c7a945c84f7d6465e6b6f30a4
sun_cx <sun.chengxiong@foxmail.com>
2020-08-16 20:21:36 +0800

    add logstream impl.

---
+++ core/LogStream.cpp
@@ -0,0 +1,182 @@
+//
+// Created by suncx on 2020/8/15.
+//
+
+#include "LogStream.h"
+
+const char Converter::digits[] = "9876543210123456789";
+const char Converter::hex_digits[] = "0123456789ABCDEF";
+const char *Converter::zero = Converter::digits + 9;
+
+template<class T>
+void LogStream::format_value(T x, size_t (*format_type)(char *, T)) {
+    if (buffer.available() >= max_size) {
+        auto len = format_type(buffer.current(), x);
+        buffer.add(len);
+    }
+}
+
+void LogStream::append(const char *data, int len) {
+    buffer.append(data, len);
+}
+
+const LogStream::Buffer &LogStream::get_buffer() const {
+    return buffer;
+}
+
+void LogStream::reset_buffer() {
+    buffer.reset();
+}
+
+LogStream &LogStream::operator<<(bool x) {
+    if (x) buffer.append("true", 4);
+    else buffer.append("false", 5);
+    return *this;
+}
+
+LogStream &LogStream::operator<<(char x) {
+    buffer.append(&x, 1);
+    return *this;
+}
+
+LogStream &LogStream::operator<<(signed char x) {
+    return *this << static_cast<char>(x);
+}
+
+LogStream &LogStream::operator<<(byte x) {
+    format_value<byte>(x, [](char *buf, byte x) -> size_t {
+        buf[0] = '0';
+        buf[1] = 'b';
+        auto len = Converter::itob(buf + 2, x);
+        return len + 2;
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(short x) {
+    format_value<short>(x, [](char *buf, short x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(unsigned short x) {
+    format_value<unsigned short>(x, [](char *buf, unsigned short x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(int x) {
+    format_value<int>(x, [](char *buf, int x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(unsigned int x) {
+    format_value<unsigned int>(x, [](char *buf, unsigned int x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(long x) {
+    format_value<long>(x, [](char *buf, long x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(unsigned long x) {
+    format_value<unsigned long>(x, [](char *buf, unsigned long x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(long long x) {
+    format_value<long long>(x, [](char *buf, long long x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(unsigned long long x) {
+    format_value<unsigned long long>(x, [](char *buf, unsigned long long x) -> size_t {
+        return Converter::itoa(buf, x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(float x) {
+    format_value<float>(x, [](char *buf, float x) -> size_t {
+        return snprintf(buf, max_size, "%.12g", x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(double x) {
+    format_value<double>(x, [](char *buf, double x) -> size_t {
+        return snprintf(buf, max_size, "%.12g", x);
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(const void *ptr) {
+    auto p = reinterpret_cast<uintptr_t>(ptr);
+    format_value<uintptr_t>(p, [](char *buf, uintptr_t pt) -> size_t {
+        buf[0] = '0';
+        buf[1] = 'x';
+        auto len = Converter::itoh(buf + 2, pt);
+        return len + 2;
+    });
+    return *this;
+}
+
+LogStream &LogStream::operator<<(const char *str) {
+    if (str == nullptr) {
+        buffer.append("(null)", 6);
+    } else {
+        buffer.append(str, strlen(str));
+    }
+    return *this;
+}
+
+LogStream &LogStream::operator<<(const byte *bytes) {
+    return *this << reinterpret_cast<const char *>(bytes);
+}
+
+LogStream &LogStream::operator<<(const string &str) {
+    buffer.append(str.c_str(), str.size());
+    return *this;
+}
+
+LogStream &LogStream::operator<<(const StringPiece &piece) {
+    buffer.append(piece.data(), piece.size());
+    return *this;
+}
+
+LogStream &LogStream::operator<<(const LogStream::Buffer &buf) {
+    return *this << buf.to_string_piece();
+}
+
+
+template<class T>
+Formatter::Formatter(const char *fmt, T val) {
+    len = snprintf(buf, sizeof(buf), fmt, val);
+}
+
+const char *Formatter::data() const {
+    return buf;
+}
+
+int Formatter::length() const {
+    return len;
+}
+
+//template Formatter::Formatter(const char *fmt, char x);
+
+
+
+
---
+++ core/LogStream.h
@@ -0,0 +1,237 @@
+//
+// Created by suncx on 2020/8/15.
+//
+
+#ifndef REACTOR_LOGSTREAM_H
+#define REACTOR_LOGSTREAM_H
+
+#include <cstddef>
+#include <string>
+#include <algorithm>
+#include "NonCopyable.h"
+#include "StringPiece.h"
+
+using std::string;
+using std::reverse;
+
+const int small_buffer = 1024 * 4;              // 4KB
+const int large_buffer = small_buffer * 1024;   // 4MB
+using byte = unsigned char;
+
+template<int size>
+class FixedBuffer final : public NonCopyable {
+private:
+    char data[size];
+    char *cur;
+
+    void (*cookie)();
+
+    const char *end() const {
+        return data + sizeof(data);
+    }
+
+    static void cookie_start();
+
+    static void cookie_end();
+
+public:
+    FixedBuffer() : cur(data) {
+        set_cookie(cookie_start);
+    }
+
+    virtual ~FixedBuffer() {
+        set_cookie(cookie_end);
+    }
+
+    void set_cookie(void (*c)()) {
+        cookie = c;
+    }
+
+    void append(const char *buf, size_t len) {
+        //TODO: append partially?
+        if (available() > len) {
+            memcpy(cur, buf, len);
+            cur += len;
+        }
+    }
+
+    int available() const { return static_cast<int>(end() - cur); }
+
+    const char *get_data() const { return data; }
+
+    int length() const { return static_cast<int >(cur - data); }
+
+    char *current() const { return cur; }
+
+    void add(size_t len) { cur += len; }
+
+    void reset() { cur = data; }
+
+    void zero() { memset(data, 0, sizeof(data)); }
+
+    //for gdb used only!
+    const char *debug_data();
+
+    string to_string() const { return string(data, length()); }
+
+    StringPiece to_string_piece() const {
+        return StringPiece(data, length());
+    }
+};
+
+class Formatter final : public NonCopyable {
+private:
+    char buf[32];
+    int len;
+public:
+    template<class T>
+    Formatter(const char *fmt, T x);
+
+    const char *data() const;
+
+    int length() const;
+};
+
+class Converter final : public NonCopyable {
+private:
+    static const char digits[];
+    static const char hex_digits[];
+    static const char *zero;
+public:
+    /**
+     * 将整数 x 转换为十进制字符串
+     * @param buf
+     * @param x
+     * @return 字符串长度
+     */
+    template<class T>
+    static size_t itoa(char buf[], T x) {
+        decltype(x) i = x;
+        char *p = buf;
+        decltype(i) lsd;
+        do {
+            lsd = i % 10;
+            i /= 10;
+            *p++ = zero[lsd];
+        } while (i != 0);
+        if (x < 0) *p++ = '-';
+        *p = '\0';
+        reverse(buf, p);
+        return p - buf;
+    }
+
+    /**
+     * 将整数 x 转换为十六进制字符串
+     * @param buf
+     * @param x
+     * @return 字符串长度
+     */
+    template<class T>
+    static size_t itoh(char buf[], T x) {
+        decltype(x) i = x;
+        char *p = buf;
+        decltype(i) lsd;
+        do {
+            lsd = i % 16;
+            i /= 16;
+            *p++ = hex_digits[lsd];
+        } while (i != 0);
+        *p = '\0';
+        reverse(buf, p);
+        return p - buf;
+    }
+
+    /**
+     * 将整数 x 转换为二进制字符串
+     * @param buf
+     * @param x
+     * @return 字符串长度
+     */
+    template<class T>
+    static size_t itob(char buf[], T x) {
+        decltype(x) i = x;
+        char *p = buf;
+        decltype(i) lsd;
+        do {
+            lsd = i % 2;
+            i /= 2;
+            *p++ = hex_digits[lsd];
+        } while (i != 0);
+        *p = '\0';
+        reverse(buf, p);
+        return p - buf;
+    }
+};
+
+class LogStream : public NonCopyable {
+private:
+    using Buffer = FixedBuffer<small_buffer>;
+
+    Buffer buffer;
+    static const int max_size = 32;
+
+    /**
+     * 数值格式化
+     * @tparam T 数值类型
+     * @param x 数值
+     * @param format_type 格式化器
+     */
+    template<class T>
+    void format_value(T x, size_t (*format_type)(char buf[], T x));
+
+    friend LogStream &operator<<(LogStream &ls, const Formatter &fmt) {
+        ls.append(fmt.data(), fmt.length());
+        return ls;
+    }
+
+public:
+
+    void append(const char *data, int len);
+
+    const Buffer &get_buffer() const;
+
+    void reset_buffer();
+
+    LogStream &operator<<(bool x);
+
+    LogStream &operator<<(char x);
+
+    LogStream &operator<<(signed char x);
+
+    LogStream &operator<<(byte x);
+
+    LogStream &operator<<(short x);
+
+    LogStream &operator<<(unsigned short x);
+
+    LogStream &operator<<(int x);
+
+    LogStream &operator<<(unsigned int x);
+
+    LogStream &operator<<(long x);
+
+    LogStream &operator<<(unsigned long x);
+
+    LogStream &operator<<(long long x);
+
+    LogStream &operator<<(unsigned long long x);
+
+    LogStream &operator<<(float x);
+
+    LogStream &operator<<(double x);
+
+    LogStream &operator<<(const void *ptr);
+
+    LogStream &operator<<(const char *str);
+
+    LogStream &operator<<(const byte *bytes);
+
+    LogStream &operator<<(const string &str);
+
+    LogStream &operator<<(const StringPiece &piece);
+
+    LogStream &operator<<(const Buffer &buf);
+
+};
+
+#endif //REACTOR_LOGSTREAM_H
