7ca09942170b28c89e4149c8d475383ad3132744
sun_cx <sun.chengxiong@foxmail.com>
2020-09-07 01:00:29 +0800

    add n-dimension MinHeap impl.

---
+++ netc/MinHeap.h
@@ -0,0 +1,101 @@
+//
+// Created by suncx on 2020/9/6.
+//
+
+#ifndef REACTOR_MINHEAP_H
+#define REACTOR_MINHEAP_H
+
+#include <vector>
+#include <stdexcept>
+
+using std::vector;
+using std::logic_error;
+
+template<class T, const int dimension = 2>
+class MinHeap final {
+private:
+    vector<T> heap;
+
+    void sift_up(int end);
+
+    void sift_down(int begin, int end);
+
+    int get_min_child_idx(int parent_idx);
+
+public:
+    void insert(const T &x);
+
+    T pop();
+
+    bool empty() const;
+
+    size_t size() const;
+};
+
+template<class T, const int dimension>
+void MinHeap<T, dimension>::insert(const T &x) {
+    heap.push_back(x);
+    sift_up(heap.size() - 1);
+}
+
+template<class T, const int dimension>
+T MinHeap<T, dimension>::pop() {
+    if (empty()) throw logic_error("can not pop an empty heap!");
+    T temp = heap.front();
+    if (size() == 1) {
+        heap.pop_back();
+        return temp;
+    }
+    heap.front() = heap.back();
+    heap.pop_back();
+    sift_down(0, heap.size() - 1);
+    return temp;
+}
+
+template<class T, const int dimension>
+void MinHeap<T, dimension>::sift_up(int end) {
+    int idx = end, parent_idx = (idx - 1) / dimension;
+    T temp = heap[idx];
+    while (idx > 0) {
+        if (heap[parent_idx] <= temp) break;
+        heap[idx] = heap[parent_idx];
+        idx = parent_idx;
+        parent_idx = (idx - 1) / dimension;
+    }
+    heap[idx] = temp;
+}
+
+template<class T, const int dimension>
+void MinHeap<T, dimension>::sift_down(int begin, int end) {
+    int idx = begin, child_idx = idx * dimension + 1;
+    T temp = heap[idx];
+    while (child_idx <= end) {
+        child_idx = get_min_child_idx(idx);
+        if (temp <= heap[child_idx]) break;
+        heap[idx] = heap[child_idx];
+        idx = child_idx;
+        child_idx = idx * dimension + 1;
+    }
+    heap[idx] = temp;
+}
+
+template<class T, const int dimension>
+int MinHeap<T, dimension>::get_min_child_idx(int parent_idx) {
+    int first_child_idx = parent_idx * dimension + 1;
+    int min_idx = first_child_idx;
+    for (int i = first_child_idx + 1; i < first_child_idx + dimension and i < heap.size(); ++i)
+        if (heap[i] < heap[min_idx]) min_idx = i;
+    return min_idx;
+}
+
+template<class T, const int dimension>
+bool MinHeap<T, dimension>::empty() const {
+    return heap.empty();
+}
+
+template<class T, const int dimension>
+size_t MinHeap<T, dimension>::size() const {
+    return heap.size();
+}
+
+#endif //REACTOR_MINHEAP_H
