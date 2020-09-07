c95ba836bf703c4c0e823f8e7f61f6eecb29c626
sun_cx <sun.chengxiong@foxmail.com>
2020-09-07 16:12:20 +0800

    add new functions.

--- netc/Socket.cpp
+++ netc/Socket.cpp
@@ -1,73 +1,35 @@
 //
-// Created by suncx on 2020/8/18.
+// Created by suncx on 2020/9/7.
 //
 
 #include "Socket.h"
 #include "Exception.h"
-#include "InetAddress.h"
 #include <unistd.h>
-#include <cstring>
-#include <netinet/tcp.h>
+#include <sys/socket.h>
 
-Socket::Socket() : sock_fd(create_socket()) {}
-
-int Socket::create_socket() const {
-    int fd = ::socket(PF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
-    if (unlikely(fd < 0)) ERROR_EXIT("socket created error.");
-    printf("create sock_fd success, sock_fd: %d\n", fd);
-    return fd;
-}
+Socket::Socket(int con_fd) : fd(con_fd) {}
 
 Socket::~Socket() {
-    auto status = close(sock_fd);
+    auto status = close(fd);
     if (unlikely(status != 0)) ERROR_EXIT("close socket error.");
 }
 
-void Socket::bind(const InetAddress &addr) const {
-    auto status = ::bind(sock_fd, addr.get_sockaddr(), sizeof(sockaddr_in));
-    if (unlikely(status != 0)) ERROR_EXIT("socket bind error.");
-}
-
-void Socket::listen() const {
-    auto status = ::listen(sock_fd, SOMAXCONN);
-    if (unlikely(status != 0)) ERROR_EXIT("socket listen error.");
-}
-
-int Socket::accept(InetAddress *peer_addr) const {
-    sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    socklen_t len = sizeof(addr);
-    int con_fd = ::accept4(sock_fd, reinterpret_cast<sockaddr *>(&addr), &len, SOCK_NONBLOCK | SOCK_CLOEXEC);
-    peer_addr->set_sockaddr(addr);
-    return con_fd;
-}
-
-void Socket::reuse_addr(bool on) const {
-    const int opt = on ? 1 : 0;
-    auto status = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    if (unlikely(status != 0)) ERROR_EXIT("setsockopt error.");
-}
-
-void Socket::tcp_no_delay(bool on) const {
-    const int opt = on ? 1 : 0;
-    auto status = setsockopt(sock_fd, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt));
-    if (unlikely(status != 0)) ERROR_EXIT("setsockopt error.");
-}
-
-void Socket::reuse_port(bool on) const {
-    const int opt = on ? 1 : 0;
-    auto status = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt));
-    if (unlikely(status != 0)) ERROR_EXIT("setsockopt error.");
+int Socket::get_fd() const {
+    return fd;
 }
 
 void Socket::keep_alive(bool on) const {
     const int opt = on ? 1 : 0;
-    auto status = setsockopt(sock_fd, SOL_SOCKET, SO_KEEPALIVE, &opt, sizeof(opt));
+    auto status = setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &opt, sizeof(opt));
     if (unlikely(status != 0)) ERROR_EXIT("setsockopt error.");
 }
 
-int Socket::fd() const {
-    return sock_fd;
+void Socket::shutdown_read() const {
+    auto status = shutdown(fd, SHUT_RD);
+    if (unlikely(status != 0)) ERROR_EXIT("shutdown_read error.");
 }
 
-
+void Socket::shutdown_write() const {
+    auto status = shutdown(fd, SHUT_WR);
+    if (unlikely(status != 0)) ERROR_EXIT("shutdown_write error.");
+}
--- netc/Socket.h
+++ netc/Socket.h
@@ -1,5 +1,5 @@
 //
-// Created by suncx on 2020/8/18.
+// Created by suncx on 2020/9/7.
 //
 
 #ifndef REACTOR_SOCKET_H
@@ -7,41 +7,20 @@
 
 #include "NonCopyable.h"
 
-class InetAddress;
-
+// 封装已连接套接字
 class Socket final : public NonCopyable {
 private:
-    const int sock_fd;  // fd 的生存期由 Socket 类控制，在 Socket 析构时会将其关闭
-
-    int create_socket() const;
-
+    const int fd;
 public:
-    Socket();
+    explicit Socket(int con_fd);
 
     virtual ~Socket();
 
-    int fd() const;
+    int get_fd() const;
 
-    void bind(const InetAddress &addr) const;
+    void shutdown_read() const;
 
-    void listen() const;
-
-    /**
-     * 接受客户端的连接，返回已连接套接字：
-     *
-     * 当连接成功时，返回已连接套接字，并设置对端的地址
-     *
-     * 连接失败返回 -1， errno 保存对应的错误码
-     * @param peer_addr 对端地址
-     * @return 已连接套接字
-     */
-    int accept(InetAddress *peer_addr) const;
-
-    void tcp_no_delay(bool on) const;
-
-    void reuse_addr(bool on) const;
-
-    void reuse_port(bool on) const;
+    void shutdown_write() const;
 
     void keep_alive(bool on) const;
 };
