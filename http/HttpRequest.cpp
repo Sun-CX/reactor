14afa270c66fdeda3a15f183882c44b2c378edb4
sun_cx <sun.chengxiong@foxmail.com>
2020-09-10 20:39:50 +0800

    add http module.

---
+++ http/HttpRequest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by suncx on 2020/9/10.
+//
+
+#include "HttpRequest.h"
---
+++ http/HttpRequest.h
@@ -0,0 +1,14 @@
+//
+// Created by suncx on 2020/9/10.
+//
+
+#ifndef REACTOR_HTTPREQUEST_H
+#define REACTOR_HTTPREQUEST_H
+
+
+class HttpRequest {
+
+};
+
+
+#endif //REACTOR_HTTPREQUEST_H
