f1ee2569ad5acd402a31b559e8dd3980d0c66bd3
sun_cx <sun.chengxiong@foxmail.com>
2020-09-11 16:30:37 +0800

    delete files.

--- netc/TimerId.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by suncx on 2020/8/18.
-//
-
-#include "TimerId.h"
-
-TimerId::TimerId() : timer_task(nullptr), sequence(0) {}
-
-TimerId::TimerId(TimerTask *task, uint32_t seq) : timer_task(task), sequence(seq) {}
