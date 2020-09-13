b17b66fb9616a35b844e1bee1b6e1176976a6318
sun_cx <sun.chengxiong@foxmail.com>
2020-09-13 21:46:40 +0800

    add code impl.

--- http/HttpRequest.cpp
+++ http/HttpRequest.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "HttpRequest.h"
+#include <cassert>
+
+HttpRequest::HttpRequest(HttpRequest::Method method, HttpRequest::Version version) : method(method), version(version) {}
+
+void HttpRequest::set_method(const char *start, const char *end) {
+    assert(method == UNKNOWN);
+    string me(start, end);
+    if (me == "GET") method = GET;
+    else if (me == "HEAD") method = HEAD;
+    else if (me == "POST") method = POST;
+    else if (me == "PUT") method = PUT;
+    else if (me == "DELETE") method = DELETE;
+    else if (me == "OPTIONS") method = OPTIONS;
+}
+
+void HttpRequest::set_version(const char *start, const char *end) {
+    assert(version == INVALID);
+    string v(start, end);
+    if (v == "HTTP/1.1") version = HTTP1_1;
+    else if (v == "HTTP/2") version = HTTP2;
+}
+
+void HttpRequest::set_path(const char *start, const char *end) {
+    path.assign(start, end);
+}
--- http/HttpRequest.h
+++ http/HttpRequest.h
@@ -5,10 +5,36 @@
 #ifndef REACTOR_HTTPREQUEST_H
 #define REACTOR_HTTPREQUEST_H
 
+#include <string>
+#include <unordered_map>
 
-class HttpRequest {
+using std::string;
+using std::unordered_map;
 
+class HttpRequest final {
+public:
+    enum Method {
+        UNKNOWN, GET, HEAD, POST, PUT, DELETE, OPTIONS
+    };
+
+    enum Version {
+        INVALID, HTTP1_1, HTTP2
+    };
+private:
+    using Headers = unordered_map<string, string>;
+
+    Method method;
+    Version version;
+    string path;
+    Headers headers;
+public:
+    explicit HttpRequest(Method method = UNKNOWN, Version version = INVALID);
+
+    void set_method(const char *start, const char *end);
+
+    void set_version(const char *start, const char *end);
+
+    void set_path(const char *start, const char *end);
 };
-
 
 #endif //REACTOR_HTTPREQUEST_H
