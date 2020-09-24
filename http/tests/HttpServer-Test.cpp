610cf556c68085dcfb39d98d57d8747a2bd3d235
sun_cx <sun.chengxiong@foxmail.com>
2020-09-24 16:28:07 +0800

    add http test case.

--- http/tests/HttpServer-Test.cpp
+++ http/tests/HttpServer-Test.cpp
@@ -2,17 +2,21 @@
 // Created by suncx on 2020/9/23.
 //
 
-#include <HttpRequest.h>
 #include "HttpServer.h"
 #include "EventLoop.h"
 #include "InetAddress.h"
 
-using std::find;
+void service(const HttpRequest &request, HttpResponse &response) {
+    response.set_response_status(200, "OK");
+    response.set_header("Content-Type","text/plain");
+    response.set_body("hello reactor");
+}
 
 int main(int argc, const char *argv[]) {
     EventLoop loop;
     InetAddress addr(true, 8080);
-    HttpServer httpServer(&loop, addr, "http-server", 3, true);
+    HttpServer httpServer(&loop, addr, "http-svr", 3, true);
+    httpServer.set_http_callback(service);
     httpServer.start();
     loop.loop();
     return 0;
