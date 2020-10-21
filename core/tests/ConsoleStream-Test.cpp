7be3371b9eb837b655083044672fa697742e5637
sun_cx <sun.chengxiong@foxmail.com>
2020-10-21 10:38:05 +0800

    add test case.

---
+++ core/tests/ConsoleStream-Test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by suncx on 2020/10/21.
+//
+
+#include "ConsoleStream.h"
+
+int main(int argc, const char *argv[]) {
+
+    LOG << "hello world.";
+    INFO << "hello world.";
+    ERROR << "hello world.";
+
+    return 0;
+}
