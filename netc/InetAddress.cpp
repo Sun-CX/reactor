11ed8fd02dc0a735e6a2321b1a267aeb37745b6b
sun_cx <sun.chengxiong@foxmail.com>
2020-10-22 18:43:23 +0800

    simplify code.

--- netc/InetAddress.cpp
+++ netc/InetAddress.cpp
@@ -12,14 +12,6 @@ InetAddress::InetAddress() {
     memset(&ad4, 0, sizeof(ad4));
 }
 
-const sockaddr *InetAddress::get_sockaddr() const {
-    return reinterpret_cast<const sockaddr *>(&ad4);
-}
-
-void InetAddress::set_sockaddr(const sockaddr_in &addr) {
-    ad4 = addr;
-}
-
 InetAddress::InetAddress(const char *ip, uint16_t port) {
     memset(&ad4, 0, sizeof(ad4));
     ad4.sin_family = AF_INET;
@@ -33,6 +25,14 @@ InetAddress::InetAddress(bool use_loop_back, uint16_t port) {
     in_addr_t ip = use_loop_back ? INADDR_LOOPBACK : INADDR_ANY;
     ad4.sin_addr.s_addr = htonl(ip);
     ad4.sin_port = htons(port);
+}
+
+const sockaddr *InetAddress::get_sockaddr() const {
+    return reinterpret_cast<const sockaddr *>(&ad4);
+}
+
+void InetAddress::set_sockaddr(const sockaddr_in &addr) {
+    ad4 = addr;
 }
 
 InetAddress InetAddress::get_local_address(int fd) {
