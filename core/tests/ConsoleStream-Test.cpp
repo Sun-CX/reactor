f5d9a2472aff7902a1438b33c5487a65e27bf85f
sun_cx <sun.chengxiong@foxmail.com>
2020-10-24 10:41:44 +0800

    add test case.

--- core/tests/ConsoleStream-Test.cpp
+++ core/tests/ConsoleStream-Test.cpp
@@ -3,12 +3,30 @@
 //
 
 #include "ConsoleStream.h"
+#include "Thread.h"
+#include "Mutex.h"
+
+static Mutex mutex;
+
+static void v1() {
+    MutexGuard guard(mutex);
+    INFO << "hello" << 1 << 6.9 << 'x' << "world" << 34 << " bye";
+}
 
 int main(int argc, const char *argv[]) {
 
-    LOG << "hello world.";
-    INFO << "hello world.";
-    ERROR << "hello world.";
+//    LOG << "hello world.";
+//    INFO << "hello world.";
+//    ERROR << "hello world.";
+
+    Thread t1(v1);
+    Thread t2(v1);
+
+    t1.start();
+    t2.start();
+
+    t1.join();
+    t2.join();
 
     return 0;
 }
