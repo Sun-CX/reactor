9e04dce163b9892e46c16358d3da188816e8cbfa
sun.chengxiong <sun.chengxiong@foxmail.com>
2021-04-09 10:52:54 +0800

    fix trivial issues.

--- core/Thread.cpp
+++ core/Thread.cpp
@@ -15,7 +15,7 @@ using std::to_string;
 
 atomic_int Thread::thread_count;
 
-pre_main void init_main_thread_title() {
+pre_main void main_thread_initialize() {
     const char *main_thread_name = "main-thread";
     int status = prctl(PR_SET_NAME, main_thread_name);
     if (unlikely(status != 0)) FATAL << "prctl error!";
